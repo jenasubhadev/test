3931fc1e79c587e863e251a47172da6e6c18061a
jenasubhadev <jenasubhadev@gmail.com>
2020-01-03 19:31:58 +0530

    Initial

---
+++ abc.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# set -ex
+# common variables
+PORT1=$PORT
+TASK=${JOB_NAME}
+REPOSITORY_URI=$AWS_ACCOUNT_ID.dkr.ecr.${REGION}.amazonaws.com/${TASK}
+echo $PORT1
+echo $REPOSITORY_URI
